f14f82e0e194595a7a45c2e31c41df5d5570eee7
hwangal77 <42627306+hwangal77@users.noreply.github.com>
2018-11-03 18:34:10 -0400

    trigger files

--- trigger.h
+++ trigger.h
@@ -8,9 +8,28 @@
 
 using namespace std;
 
+class condition{
+ public:
+  string object;
+  string owner;
+  string has;
+  string status;
+  bool validated;
+
+  bool validate(condition conditioner, string object, string owner, string has, string status);
+};
+
 class trigger{
  public:
-  string condition;
+  condition conditioner;
+  string print;
+  vector<string> action;
+  string command;
+  string type;
+  bool valid = 1;
+
+  void printTrigger(trigger triggered, string command);
+  bool setValid(trigger triggered);
 };
 
 #endif
