b21a55a90cc9fcca77724cfc0669d875796344a4
Lrajoo <Lrajoo@purdue.edu>
2018-11-02 21:11:18 -0400

    Rooms work

--- main.cpp
+++ main.cpp
@@ -6,28 +6,79 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include "room.h"
+
 #include "rapidxml-1.13/rapidxml.hpp"
+#include "rapidxml-1.13/rapidxml_print.hpp"
+#include "rapidxml-1.13/rapidxml_utils.hpp"
+//#include "rapidxml-1.13/rapidxml_iterators.hpp"
 
 using namespace rapidxml;
 using namespace std;
 
 int main(int argc, char * argv[])
-{
-    cout << "Parsing now" << endl;
-    
+{   
+    //xml intialization for parsing
     xml_document<> doc;
-    xml_node<> * root_node;
+    //input file to be parsed
     ifstream file("roomsample.xml");
     stringstream buffer;
     buffer << file.rdbuf();
     file.close();
     string content(buffer.str());
     doc.parse<0>(&content[0]);
-    
-    root_node = doc.first_node("map");
-    xml_node<> * room_node = root_node->first_node("room");
-    xml_node<> * name_node = room_node->first_node("name");
-    cout << name_node->value() << endl; 
+    //vectors for room, item, container and creature 
+    vector<room> room_vector;
+    vector<xml_node<> * > item_vector;
+    vector<xml_node<> * > container_vector;
+    vector<xml_node<> * > creature_vector; 
+    //root node
+    xml_node<> * root_node = doc.first_node("map");
+    //scanning through file and identifying rooms, items, containers and creatures
+    for(xml_node<> * curr_node = root_node->first_node(); curr_node; curr_node = curr_node->next_sibling()){
+        room roomy;
+        if(string(curr_node->name()) == "room"){
+            roomy.name = curr_node->first_node("name")->value();
+            roomy.description = curr_node->first_node("description")->value();
+            if(curr_node->first_node("type")){
+                roomy.type = curr_node->first_node("type")->value();
+            }
+            for(xml_node<> * border = curr_node->first_node("border"); border; border = border->next_sibling()){
+                if(string(border->value()) != "exit"){
+                    if(string(border->first_node("direction")->value()) == "north"){
+                        roomy.north = border->first_node("name")->value();
+                        cout << "n" << endl;
+                    }
+                    if(string(border->first_node("direction")->value()) == "south"){
+                        roomy.south = border->first_node("name")->value();
+                        cout << "s" << endl;
+                    }
+                    if(string(border->first_node("direction")->value()) == "east"){
+                        roomy.east = border->first_node("name")->value();
+                        cout << "e" << endl;
+                    }
+                    if(string(border->first_node("direction")->value()) == "west"){
+                        roomy.west = border->first_node("name")->value();
+                        cout << "w" << endl;
+                    }
+                }
+
+            
+            }
+            room_vector.push_back(roomy);
+        }
+    }
+
+    // int size = room_vector.size();
+    // int i = 0;
+    // while( i < size){
+    //     room curr = room_vector[i];
+    //     curr.getName(curr);
+    //     curr.getDescription(curr);
+    //     curr.getType(curr);
+    //     curr.getBorders(curr);
+    //     i++;
+    // }
 
     return 0;
 }
