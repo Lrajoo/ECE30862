afb0dbbdf4760658a87fae629c37778513108b02
hwangal77 <42627306+hwangal77@users.noreply.github.com>
2018-11-03 16:19:43 -0400

    container files

---
+++ trigger.h
@@ -0,0 +1,16 @@
+#ifndef TRIGGER_H
+#define TRIGGER_H
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+class trigger{
+ public:
+  string condition;
+};
+
+#endif
